f7fd643500371a6888358a70e7ecb0b89a9477cf
Test <idv14c.cortiz@uartesdigitales.edu.mx>
2018-01-27 00:11:44 -0600

    Menu termiado
    Se acabó el menú del juego.

---
+++ Assets/Lalo/Scripts/ED_MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ED_MenuManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame_Bttn()
+    {
+        SceneManager.LoadScene("Game_Scene");
+    }
+
+    public void Credits_Bttn()
+    {
+        SceneManager.LoadScene("Credits_Scene");
+    }
+
+    public void Exit_Bttn()
+    {
+        Application.Quit();
+    }
+}
